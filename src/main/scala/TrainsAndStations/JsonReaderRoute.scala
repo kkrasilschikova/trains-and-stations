a79394586c987a8de7fa50083098a4d2f33242ed
kkrasilschikova <kseniya.krasilschikova@gmail.com>
2018-01-23 15:38:16 +0300

    add json deserializer for route

---
+++ src/main/scala/TrainsAndStations/JsonReaderRoute.scala
@@ -0,0 +1,25 @@
+package TrainsAndStations
+
+import play.api.libs.json.JsValue
+
+trait JsonReaderRoute[A] {
+  def read(value: A): Route
+}
+
+object JsonReaderRouteInstances {
+  implicit val routeReader: JsonReaderRoute[JsValue] = new JsonReaderRoute[JsValue] {
+    def read(value: JsValue): Route = {
+      val stations = (value \ "route").as[List[Int]] match {
+        case list => list.map(x => Station(x))
+      }
+      Route(stations)
+    }
+  }
+}
+
+object JsonReaderRouteSyntax {
+  implicit class JsonReaderOps[A](value: A) {
+    def toRoute(implicit r: JsonReaderRoute[A]): Route =
+      r.read(value)
+  }
+}
